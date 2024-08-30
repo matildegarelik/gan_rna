9f48b4ccae5477a0eae4509313526ae926ff5f2d
matildegarelik <garelikmatilde@gmail.com>
2024-08-30 20:24:19 -0300

    error en losss

--- train.py
+++ train.py
@@ -5,12 +5,15 @@ from torch.optim import Adam
 from utils import continuous_to_one_hot, generate_latent_space_samples, one_hot_to_rna_with_padding
 from model import padding_loss
 
-def train(generator, discriminator, train_loader, loss_function, optimizer_discriminator, optimizer_generator, num_epochs, device, latent_dim, max_seq_length, mu,log_filename, sequences_filename):
+def train(generator, discriminator, train_loader, loss_function, optimizer_discriminator, optimizer_generator, num_epochs, device, latent_dim, max_seq_length, mu,log_filename, sequences_filename, gen_filename):
     for epoch in range(num_epochs):
         all_real_samples = []
         all_generated_samples = []
         all_loss_discriminator_real = []
         all_loss_discriminator_generated = []
+        all_loss_individual_generator = []
+        all_padding_loss_generator = []
+        all_all_generated_samples = []
 
         for n, (real_samples, _) in enumerate(train_loader):
             real_samples = real_samples.to(device)
@@ -28,8 +31,7 @@ def train(generator, discriminator, train_loader, loss_function, optimizer_discr
             generator.eval()
             discriminator.zero_grad()
             generator.zero_grad()
-            #output_discriminator = discriminator(all_samples)
-            #loss_discriminator = loss_function(output_discriminator, all_samples_labels)
+            
             # Salida del discriminador para las muestras reales y generadas por separado
             output_discriminator_real = discriminator(real_samples)
             output_discriminator_generated = discriminator(generated_samples_one_hot)
@@ -46,7 +48,7 @@ def train(generator, discriminator, train_loader, loss_function, optimizer_discr
             discriminator.eval()
             generator.train()
             # entrenamiento del generador (5 veces x epoca del discriminador)
-            for _ in range(5):
+            for _ in range(50):
                 discriminator.zero_grad()
                 generator.zero_grad()
                 latent_space_samples, random_lengths = generate_latent_space_samples(real_samples.size(0), max_seq_length, device)
@@ -57,16 +59,20 @@ def train(generator, discriminator, train_loader, loss_function, optimizer_discr
                 output_discriminator_generated = discriminator(generated_samples_one_hot)
 
                 # pérdida del generador
-                padding_loss_value = padding_loss(generated_samples, random_lengths, device)
+                for i,s in enumerate(generated_samples):
+                    padding_loss_value = padding_loss(s.unsqueeze(0), [random_lengths[i]], device)
+                    all_padding_loss_generator.append(padding_loss_value.detach().cpu().numpy())
                 #loss_generator = loss_function(output_discriminator_generated, real_samples_labels) + mu * padding_loss_value
-                loss_generator_individual = loss_function(output_discriminator_generated, real_samples_labels) + mu * padding_loss_value
-                loss_generator = loss_generator_individual.mean()
+                padding_loss_value = padding_loss(generated_samples, random_lengths, device)
+
+                loss_generator_individual = loss_function(output_discriminator_generated, real_samples_labels) 
+                padding_loss_individual = mu * padding_loss_value
+                loss_generator = (loss_generator_individual + padding_loss_individual).mean()
                 loss_generator.backward()
                 optimizer_generator.step()
-
-                # guardo ambos terminos del loss del generador en un csv
-                # llevar 5 a 50, 100
-                # distribuir mas equitativo probabilidades
+                
+                all_loss_individual_generator.append(loss_generator_individual.detach().cpu().numpy())
+                all_all_generated_samples.append(generated_samples_one_hot.cpu().numpy())
 
             all_real_samples.append(real_samples.cpu().numpy())
             all_generated_samples.append(generated_samples_one_hot.cpu().numpy())
@@ -97,3 +103,15 @@ def train(generator, discriminator, train_loader, loss_function, optimizer_discr
                             
                             # Guardar la secuencia generada y la pérdida asociada
                             seq_writer.writerow([epoch, generated_rna_seq, "Generated", all_loss_discriminator_generated[i][j].item()])
+            
+            with open(gen_filename, mode='a', newline='') as gen_seq_file:
+                gen_seq_writer = csv.writer(gen_seq_file)
+                with torch.no_grad():
+                    for i in range(len(all_generated_samples)):
+                        for j in range(all_generated_samples[i].shape[0]):
+                            generated_rna_seq = one_hot_to_rna_with_padding(all_all_generated_samples[i][j])
+                            
+                            individual_loss = float(all_loss_individual_generator[i][j])
+                            padding_loss_g = float(all_padding_loss_generator[i])
+                            
+                            gen_seq_writer.writerow([epoch, generated_rna_seq, individual_loss, padding_loss_g])
